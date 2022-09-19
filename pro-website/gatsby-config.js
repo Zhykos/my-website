ce2fada5b40b3745b46d96708a0f5668503dca79
Thomas Cicognani <tcicognani@norsys.fr>
2022-09-19 18:27:14 +0200

    fix(gatsby): remove unused feed dependency

--- pro-website/gatsby-config.js
+++ pro-website/gatsby-config.js
@@ -224,13 +224,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
-      },
-    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
