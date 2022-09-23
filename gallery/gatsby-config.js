6315b3104e32753b5b99ecf8542536b2885f1fbf
Thomas Cicognani <tcicognani@norsys.fr>
2022-09-23 12:39:35 +0200

    fix: oops I forgot to add the Gatsby config in the repo

---
+++ gallery/gatsby-config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  siteMetadata: {
+    // Site URL for when it goes live
+    siteUrl: `https://videogamegallery.zhykos.fr/`,
+    // Your Name
+    name: 'VGG: Video Game Gallery',
+    // Main Site Title
+    title: `Thomas Cicognani | Ingénieur concepteur`,
+    // Description that goes under your name in main bio
+    description: `My own little museum about video games.`,
+    // Optional: Twitter account handle
+    //author: `@rfitzio`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+              wrapperStyle: `margin: 0 0 30px;`,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `devfolio`,
+        short_name: `devfolio`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`, // This color appears on mobile
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
+};
