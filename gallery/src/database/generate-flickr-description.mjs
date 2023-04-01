ff878da743771bd933e413de303a6bfa263a46cd
Thomas Cicognani <github@tomtom.email>
2023-04-01 13:11:45 +0200

    feat(galerie): copie dans le presse papier du résultat du générateur
    + passage en pur JS car sinon c'est relou

---
+++ gallery/src/database/generate-flickr-description.mjs
@@ -0,0 +1,132 @@
+import clipboard from 'clipboardy';
+import inquirer from 'inquirer';
+
+const platforms = ['PC', 'Xbox One'];
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'type',
+    message: "Type d'album ?",
+    choices: ['Jeu vidéo', 'Événement'],
+  },
+  {
+    type: 'input',
+    name: 'slug',
+    message: 'Slug IGDB ?',
+    when(answers) {
+      return answers.type === 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'list',
+    name: 'origin',
+    message: "Plateforme d'origine ?",
+    choices: platforms,
+    when(answers) {
+      return answers.type === 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'list',
+    name: 'played',
+    message: 'Joué sur quelle plateforme ?',
+    choices: platforms,
+    when(answers) {
+      return answers.type === 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'list',
+    name: 'version',
+    message: 'Version ?',
+    choices: ['released'],
+    when(answers) {
+      return answers.type === 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'input',
+    name: 'event',
+    message: "Nom de l'événement ?",
+    when(answers) {
+      return answers.type !== 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'input',
+    name: 'year',
+    message: "Année de l'événement ?",
+    when(answers) {
+      return answers.type !== 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'input',
+    name: 'country',
+    message: "Pays de l'événement ?",
+    when(answers) {
+      return answers.type !== 'Jeu vidéo';
+    },
+  },
+  {
+    type: 'input',
+    name: 'city',
+    message: "Ville de l'événement ?",
+    when(answers) {
+      return answers.type !== 'Jeu vidéo';
+    },
+  },
+]).then((answers) => {
+  console.log('Description générée :\n');
+  let description = null;
+
+  if (answers.type === 'Jeu vidéo') {
+    const vggMeta = `"t":"g","s":"${
+      answers.slug
+    }","p":"${mapPlatformIntoVggMeta(
+      answers.played
+    )}","o":"${mapPlatformIntoVggMeta(
+      answers.origin
+    )}","v":"${mapVersionIntoVggMeta(answers.version)}"`;
+
+    const encodedVggMeta = Buffer.from(vggMeta).toString('base64');
+
+    description = `Screenshots from video game.
+${answers.origin} and ${answers.version} version. Played on ${answers.played}.
+vgg-meta:
+${encodedVggMeta}`;
+  } else {
+    const vggMeta = `"t":"e","y":${answers.year},"c":"${answers.country}","cy":"${answers.city}","n":"${answers.event}"`;
+
+    const encodedVggMeta = Buffer.from(vggMeta).toString('base64');
+
+    description = `Photos from an event.
+${answers.country}, ${answers.city}.
+vgg-meta:
+${encodedVggMeta}`;
+  }
+
+  console.log(description + '\n');
+  clipboard.writeSync(description);
+});
+
+function mapPlatformIntoVggMeta(platform) {
+  switch (platform) {
+    case 'Xbox One':
+      return 'xboxone';
+    case 'PC':
+      return 'win';
+    default:
+      throw new Error(`Plateforme '${platform}' non gérée.`);
+  }
+}
+
+function mapVersionIntoVggMeta(version) {
+  switch (version) {
+    case 'released':
+      return 'r';
+    default:
+      throw new Error(`Version '${version}' non gérée.`);
+  }
+}
